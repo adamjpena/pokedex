45af3255fc377393c34d2f8ab9899111b9c9afc9
Adam Peña <adamjpena@gmail.com>
2022-09-09 19:30:28 -0500

    remove boilerplate & create initial app structure

---
+++ src/components/PokemonList/PokemonList.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './PokemonList.module.scss';
+
+const PokemonList = () => {
+  return <div className={styles.pokemonList}>Foobar</div>;
+};
+
+export default PokemonList;

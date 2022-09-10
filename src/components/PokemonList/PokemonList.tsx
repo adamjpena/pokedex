7f8940a1d14c24571ab93be04ec34bf8ab88961e
Adam Peña <adamjpena@gmail.com>
2022-09-09 19:51:08 -0500

    show list of pokemon

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getPokemonList } from '../../services/pokemonList';
 import styles from './PokemonList.module.scss';
 
+import { Pokemon } from '../../global/types';
+import { resourceLimits } from 'worker_threads';
+
 const PokemonList = () => {
-  return <div className={styles.pokemonList}>Foobar</div>;
+  const [pokemonListData, setPokemonListData] = useState<Pokemon[]>([]);
+  useEffect(() => {
+    getPokemonList().then(({ results }: { results: Pokemon[] }) => {
+      setPokemonListData(results);
+    });
+  });
+
+  return (
+    <ul className={styles.pokemonList}>
+      {pokemonListData.length > 0 &&
+        pokemonListData.map(({ name }) => <li key={name}>{name}</li>)}
+    </ul>
+  );
 };
 
 export default PokemonList;

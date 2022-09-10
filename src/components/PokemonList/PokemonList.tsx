c5b3519721a06259ee9858cb071084a9907c41fc
Adam Peña <adamjpena@gmail.com>
2022-09-09 20:09:56 -0500

    enable pagination

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -3,21 +3,41 @@ import { getPokemonList } from '../../services/pokemonList';
 import styles from './PokemonList.module.scss';
 
 import { Pokemon } from '../../global/types';
-import { resourceLimits } from 'worker_threads';
 
 const PokemonList = () => {
   const [pokemonListData, setPokemonListData] = useState<Pokemon[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+
+  const decreasePage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const increasePage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
   useEffect(() => {
-    getPokemonList().then(({ results }: { results: Pokemon[] }) => {
-      setPokemonListData(results);
-    });
-  });
+    getPokemonList({ currentPage }).then(
+      ({ results }: { results: Pokemon[] }) => {
+        setPokemonListData(results);
+      },
+    );
+    console.log('s');
+  }, [currentPage]);
 
   return (
-    <ul className={styles.pokemonList}>
-      {pokemonListData.length > 0 &&
-        pokemonListData.map(({ name }) => <li key={name}>{name}</li>)}
-    </ul>
+    <>
+      <ul className={styles.pokemonList}>
+        {pokemonListData.length > 0 &&
+          pokemonListData.map(({ name }) => <li key={name}>{name}</li>)}
+      </ul>
+      <div className={styles.pagination}>
+        <button disabled={currentPage === 0} onClick={decreasePage}>
+          Previous
+        </button>
+        <button onClick={increasePage}>Next</button>
+      </div>
+    </>
   );
 };
 
--- src/services/pokemonList.ts
+++ src/services/pokemonList.ts
@@ -1,5 +1,6 @@
 const URL = 'https://pokeapi.co/api/v2/pokemon';
 
-export const getPokemonList = () => {
-  return fetch(URL).then((response) => response.json());
+export const getPokemonList = ({currentPage = 0, limit = 20}: {currentPage?: number, limit?: number}) => {
+  const params = `?limit=${limit}&offset=${limit * currentPage}`
+  return fetch(`${URL}${params}`).then((response) => response.json());
 };
